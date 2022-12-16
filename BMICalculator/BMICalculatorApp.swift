a996c955c89ffea3ba0558f8aeb10b0b71065640
Hafiz Shaikh <Shaikhhafizf@gmail.com>
2022-12-15 23:13:09 -0500

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Hafiz Shaikh on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
